2720d3eae0fa9e9acfac8857b5255b2e2d643684
Jonathan MirCha <jonmircha@gmail.com>
2017-12-16 08:58:38 -0600

    Clase 2/5 Fundamentos JS concepto de objetos

--- js/tipos_de_datos.js
+++ js/tipos_de_datos.js
@@ -52,3 +52,55 @@ console.log('Tipos complejos')
 console.log('Funciones')
 saludo('Jonathan')
 saludo()
+
+//Diferencia entre let y const
+const PI = 3.141592653589793
+
+//Cuando usamos const forzozamente hay que asignar un valor a la variable
+//const unaConstante
+
+console.log( PI )
+//Una vez que una variable de tipo const se le asigna valor, éste no podrá cambiar
+//PI = 3.1416
+
+//Declaración de objetos
+const libro = {
+  titulo: 'Ready Player One',
+  autor: 'Ernest Cline',
+  anio: 2011,
+  editorial: 'Ediciones B',
+  citar: function () {
+    return `Ficha bibliográfica: ${this.titulo}. ${this.autor}. Editorial ${this.editorial}, ${this.anio}.`
+  }
+}
+
+console.log(
+  'Objetos',
+  '\n',
+  libro,
+  '\n',
+  libro.titulo,
+  '\n',
+  libro.autor,
+  '\n',
+  libro.anio,
+  '\n',
+  libro.editorial,
+  '\n',
+  libro.citar()
+)
+
+libro.autor = {
+  nombre: 'Ernest',
+  apellido: 'Cline'
+}
+
+console.log(
+  libro,
+  '\n',
+  libro.autor,
+  '\n',
+  libro.autor.nombre,
+  '\n',
+  libro.autor.apellido
+)
