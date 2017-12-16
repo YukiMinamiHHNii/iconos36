1255d69b80c2ee137a50e7cdcc6ad6a6e87df8ef
Jonathan MirCha <jonmircha@gmail.com>
2017-12-16 11:56:54 -0600

    Objetos literales terminado

--- js/objetos.js
+++ js/objetos.js
@@ -72,4 +72,61 @@
 (function (c, d) {
   c('*****Clases de POO*****')
 
+  //Una clase es un modelo a seguir, un machote
+  class Perro {
+    //El constructor es un método especial que se ejecuta en el momento de instanciar la clase
+    constructor ( nombre, edad, esterilizado, raza, genero ) {
+      //Definición de los Atributos de mi clase
+      this.nombre = nombre
+      this.edad = edad
+      this.esterilizado = esterilizado
+      this.raza = raza
+      this.genero = genero
+    }
+
+    //Definición de los métodos de mi clase
+    ladrar () {
+      c('Guauuu guauuu!!!')
+    }
+
+    comer ( comida = 'croquetas' ) {
+      c(`${this.nombre} come ${comida}`)
+      c(this.nombre + ' come ' + comida)
+    }
+
+    aparecer ( imagen ) {
+      d.write('<img src="' + imagen + '">')
+      d.write(`<img src="${imagen}">`)
+    }
+  }
+
+  const firulais = new Perro('Firulais', 3, true, 'Callejero', 'Macho')
+
+  c(
+    firulais,
+    firulais.nombre,
+    firulais.edad,
+    firulais.esterilizado,
+    firulais.raza,
+    firulais.genero
+  )
+
+  firulais.ladrar()
+  firulais.comer('Carne')
+  firulais.aparecer('http://cinicosdesinope.com/wp-content/uploads/2015/09/firulais-perro-mascota-rugrats-personajes.png')
+
+  const frida = new Perro('Frida', 7, true, 'Labrador', 'Hembra')
+
+  c(
+    frida,
+    frida.nombre,
+    frida.edad,
+    frida.esterilizado,
+    frida.raza,
+    frida.genero
+  )
+
+  frida.ladrar()
+  frida.comer()
+  frida.aparecer('https://o.aolcdn.com/images/dims?crop=2364%2C3246%2C0%2C0&quality=85&format=jpg&resize=900%2C1236&image_uri=http%3A%2F%2Fo.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2F3aa22b4c66e6368bdd04f368deab49d6%2F205731066%2FDSC_8215.JPG&client=a1acac3e1b3290917d92&signature=39c3a0c8a4f94b2f3bd71ba723559c988e183df0')
 })(console.log, document);
