1f5e5b49e41a2fa603091829a3218ff15b5775e8
Jonathan MirCha <jonmircha@gmail.com>
2017-12-16 11:29:46 -0600

    Objetos literales terminado

--- js/objetos.js
+++ js/objetos.js
@@ -34,4 +34,42 @@
   perro.comer()
   perro.comer('tacos')
   perro.aparecer('https://jonmircha.github.io/edjs-paradigmas2017/img/kEnAi.jpg')
+
+  const perro2 = {
+    nombre: 'Boni',
+    edad: 10,
+    esterilizado: false,
+    raza: 'Boxer',
+    genero: 'Hembra',
+    ladrar: function () {
+      c('Guauuu guauuu!!!')
+    },
+    comer: function (comida = 'croquetas') {
+      c(`${this.nombre} come ${comida}`)
+      c(this.nombre + ' come ' + comida)
+    },
+    aparecer: function (imagen) {
+      d.write('<img src="' + imagen + '">')
+      d.write(`<img src="${imagen}">`)
+    }
+  }
+
+  c(
+    perro2,
+    perro2.nombre,
+    perro2.edad,
+    perro2.esterilizado,
+    perro2.raza,
+    perro2.genero
+  )
+
+  perro2.ladrar()
+  perro2.comer()
+  perro2.comer('tacos')
+  perro2.aparecer('https://i1.wp.com/www.mundoperro.net/wp-content/uploads/boxer-perro-guardian.jpg?w=822&h=564&crop&ssl=1')
+})(console.log, document);
+
+(function (c, d) {
+  c('*****Clases de POO*****')
+
 })(console.log, document);
