979970765e93e818ef9e92cabf23da634a230a86
Jonathan MirCha <jonmircha@gmail.com>
2018-01-06 09:16:17 -0600

    Subiendo recursos ejercicio DOM

---
+++ js/dom.js
@@ -0,0 +1,15 @@
+;
+(function (c, d) {
+  /* ********** IMPORTACIÓN DE MÓDULOS ********** */
+
+  /* ********** DECLARACIÓN DE VARIABLES ********** */
+
+  /* ********** DECLARACIÓN DE FUNCIONES ********** */
+
+  /* ********** EJECUCIÓN DE CÓDIGO ********** */
+  //Manejo de selectores CSS con JS
+  c(
+    d.querySelector('li'),
+    d.querySelector('#theory')
+  )
+})(console.log, document);
