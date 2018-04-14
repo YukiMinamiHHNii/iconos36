5449ac5ab5b3bbe21495b10630f29f6eaa05a38e
Jonathan MirCha <jonmircha@gmail.com>
2018-04-14 12:58:34 -0500

    Contenido Clase 5 Interfaces

---
+++ PHP_MySQL/crud/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD con MySQL, PHP y AJAX</title>
+  <link rel="stylesheet" href="./crud.css">
+</head>
+<body>
+  <h1>CRUD con MySQL, PHP y AJAX</h1>
+  <a href="../#ejercicios">regresar</a>
+  <main class="CRUD">
+    <header class="CRUD-header">
+      <h2>Lista de Géneros</h2>
+      <a href="#add" class="u-btn  u-add">agregar</a>
+    </header>
+    <aside class="CRUD-response">
+      <div class="u-preload">
+        <div class="spinner">
+          <div class="rect1"></div>
+          <div class="rect2"></div>
+          <div class="rect3"></div>
+          <div class="rect4"></div>
+          <div class="rect5"></div>
+        </div>
+      </div>
+      <div class="u-message">
+        Mensaje del Servidor
+      </div>
+    </aside>
+    <table class="CRUD-table">
+      <tr>
+        <th>genre_id</th>
+        <th>genre_name</th>
+        <th>editar</th>
+        <th>borrar</th>
+      </tr>
+    </table>
+  </main>
+  <template id="table-row">
+    <tr>
+      <td class="genre_id"></td>
+      <td class="genre_name"></td>
+      <td>
+        <a href="#edit" class="u-btn  u-edit">editar</a>
+      </td>
+      <td>
+        <a href="#delete" class="u-btn  u-delete">eliminar</a>
+      </td>
+    </tr>
+  </template>
+  <script src="./crud.js"></script>
+</body>
+</html>
